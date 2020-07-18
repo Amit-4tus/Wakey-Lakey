8ad586569003d0fa3b752bd60e712d797d13fb40
Amit Fortus <amit.fortus4@gmail.com>
2020-07-18 16:27:18 +0300

    first commit

---
+++ Assets/FirstPersion AIO Pack/FirstPersonAIO/PickUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public Transform inFront;
+    public Boolean isPickedUp = false;
+
+     void pickUp()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("e") && !isPickedUp)
+        {
+            GetComponent<CapsuleCollider>().enabled = false;
+            GetComponent<Rigidbody>().useGravity = false;
+            GetComponent<Rigidbody>().freezeRotation = true;
+            this.transform.position = inFront.position;
+            this.transform.parent = GameObject.Find("InFront").transform;
+            isPickedUp = true;
+        } else if (Input.GetKeyDown("e") && isPickedUp)
+        {
+            GetComponent<CapsuleCollider>().enabled = true;
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<Rigidbody>().freezeRotation = false;
+            this.transform.parent = null;
+            isPickedUp = false;
+        }
+    }
+}
